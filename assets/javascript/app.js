97bf2ad1f70f95ff3ea3398421dbc0e84f9adf9f
Claire Stolp <clairemstolp@gmail.com>
2018-05-25 21:57:51 -0400

    tried storing arr as a jQuery object, see console for error

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,24 +5,40 @@ var data;
 var offset = 0;
 
 function addCard (gif) {
+    var container = $('<div>')
+        .addClass('grid-item');
 
-    var img = $('<img>', {class: 'grid-item', src: gif.images.downsized_still.url, alt: gif.slug})
+    var img = $('<img>', {src: gif.images.downsized_still.url, alt: gif.slug})
+        .addClass('result-img')
         .attr('data-still', gif.images.downsized_still.url)
         .attr('data-animated', gif.images.downsized.url)
         .attr('data-state', 'still');
+    
+    var card = $('<div>')
+        .addClass('result-item');
+    var title = $('<p>')
+        .text(gif.title)
+        .addClass('result-text')
+        .css('font-weight', 'bold');
+    var rating = $('<p>')
+        .text(gif.rating)
+        .addClass('result-text');
+    
+    card
+        .append(img)
+        .append(title)
+        .append(rating);
 
-    return img;
+    container.append(card);
+    return container;
 }
 
-//when aside btn is clicked, ajax call
-    //document.on('click', '.aside-btn', callback)
-    //prepend results to #results
-//add btn to clear results div
+
 
 
 topics.forEach(function (item, index) {
     var btn = $('<button>')
-        .addClass('btn btn-primary m-2 aside-btn')
+        .addClass('aside-btn')
         .val(item)
         .text(item);
     aside.append(btn);
@@ -38,6 +54,13 @@ $('#addBtn').on('click', function () {
     aside.prepend(btn);
 });
 
+var $grid = $('.grid').masonry({
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    percentPosition: true,
+    horizontalOrder: true
+  });
+
 $(document).on('click', '.aside-btn', function () {
     var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=zlEsPY1h8Xym7X61wEe9RncZ973KIsCt&q=${$(this).val()}&limit=12&offset=${offset}&rating=PG&lang=en`;
     console.log(queryURL);
@@ -47,20 +70,28 @@ $(document).on('click', '.aside-btn', function () {
     }).then(function(response){
         data = response.data;
         console.log(data);
-        
-        $(data).each(function(i, gif){
-            $('.grid').append(addCard(gif));
-        }); 
 
-        var $grid = $('.grid').imagesLoaded().progress(function() {
-            $('.grid').masonry({
-              itemSelector: '.grid-item',
-              columnWidth: '.grid-sizer',
-              percentPosition: true,
-              horizontalOrder: true
-            });
-          });
+        $grid.masonry();
+        var arr = [];
+        $(data).each(function(i, gif){
+            var card = addCard(gif)
+                .addClass('grid-item');
+            arr.push(card);
+        });
+
+        var $arr = $(arr);
+        console.log('$arr', $(arr));
+
+        $grid
+            .append($arr)
+            .masonry('appended', $arr)
+            .masonry();
         
+        
+
+        
+        //$('#grid').masonry( 'remove', $('#grid').find('.grid-item') ).masonry('layout');
+
 
         offset += 12;
         console.log(results);
@@ -68,7 +99,7 @@ $(document).on('click', '.aside-btn', function () {
 });
 
 $(document).on('click', '.grid-item', function () {
-    var img = $(this);
+    var img = $(this).find('img');
     var imgState = img.attr('data-state');
     if(imgState === 'still'){
         img
