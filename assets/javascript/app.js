50daf20fba0be1e294f8b0b1301e7af223de51c7
Claire Stolp <clairemstolp@gmail.com>
2018-05-28 19:52:55 -0400

    fixed masonry: and refactored code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@ var aside = $('#topics');
 var data;
 var offset = 0;
 
-function addCard (gif) {
+/* function addCard (gif) {
     var container = $('<div>')
         .addClass('grid-item');
 
@@ -31,8 +31,66 @@ function addCard (gif) {
 
     container.append(card);
     return container;
-}
+}*/
 
+function addCard(gif) {
+    var elem = document.createElement('div')
+        elem.setAttribute('class', 'grid-item');
+
+    var stillImg =  gif.images.downsized_still.url;
+    var animatedImg =  gif.images.downsized.url;
+    var img = document.createElement('img');
+        img.setAttribute('class', 'result-img');
+        img.setAttribute('src', stillImg);
+        img.setAttribute('data-still', stillImg);
+        img.setAttribute('data-animated',animatedImg);
+        img.setAttribute('data-state', 'still');
+    
+    var card = document.createElement('div');
+        card.setAttribute('class', 'result-item');
+
+    var title = document.createElement('p');
+        title.innerText = gif.title;
+        title.setAttribute('class', 'result-text');
+
+    var rating = document.createElement('p');
+        rating.innerText = gif.rating;
+        rating.setAttribute('class', 'result-text');
+
+    card.append(img);
+    card.append(title);
+    card.append(rating);
+
+    elem.append(card);
+    return elem;
+};
+
+function appendCard(response){
+    data = response.data;
+    console.log(data);
+
+    var arr = [];
+
+    var $grid = $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-sizer',
+      });
+
+    $(data).each(function(i, gif){
+        arr.push(addCard(gif));
+    });
+
+    $grid
+        .append(arr)
+        .masonry('appended', arr)
+        .masonry('layout');
+    
+    $grid.imagesLoaded().progress(function () {
+        $grid.masonry();
+    });
+
+    console.log(results);
+}
 
 
 
@@ -54,48 +112,14 @@ $('#addBtn').on('click', function () {
     aside.prepend(btn);
 });
 
-var $grid = $('.grid').masonry({
-    itemSelector: '.grid-item',
-    columnWidth: '.grid-sizer',
-    percentPosition: true,
-    horizontalOrder: true
-  });
-
 $(document).on('click', '.aside-btn', function () {
     var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=zlEsPY1h8Xym7X61wEe9RncZ973KIsCt&q=${$(this).val()}&limit=12&offset=${offset}&rating=PG&lang=en`;
     console.log(queryURL);
     $.ajax({
         url: queryURL,
-        method: 'GET'
-    }).then(function(response){
-        data = response.data;
-        console.log(data);
-
-        $grid.masonry();
-        var arr = [];
-        $(data).each(function(i, gif){
-            var card = addCard(gif)
-                .addClass('grid-item');
-            arr.push(card);
-        });
-
-        var $arr = $(arr);
-        console.log('$arr', $(arr));
-
-        $grid
-            .append($arr)
-            .masonry('appended', $arr)
-            .masonry();
-        
-        
-
-        
-        //$('#grid').masonry( 'remove', $('#grid').find('.grid-item') ).masonry('layout');
-
-
-        offset += 12;
-        console.log(results);
-    });
+        method: 'GET',
+        success: appendCard
+    }).then(() => offset += 12);
 });
 
 $(document).on('click', '.grid-item', function () {
